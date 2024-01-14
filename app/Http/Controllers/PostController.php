bfedbd0a29fb0dbf9c2cd6213675db93c6dec568
Ryxena <rubbenme@proton.me>
2024-01-14 11:03:50 +0700

    Adding Post Controller (Ki-Week 8)

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $post = Post::latest()->paginate(10);
+        return view('index', compact('post'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($lang, $slug)
+    {
+        if (!in_array($lang, ['en', 'id'])) {
+            abort(404);
+        }
+        App::setLocale($lang);
+        $post = Post::where('slug', $slug)->firstOrFail();
+        return view('Post.post', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
