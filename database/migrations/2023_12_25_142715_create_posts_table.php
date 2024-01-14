b1d3f55e150d02a5ef69e84bf9a1cb4448e5a132
Ryxena <rubbenme@proton.me>
2024-01-14 11:17:46 +0700

    Create Post table (Ki-Week 8)

---
+++ database/migrations/2023_12_25_142715_create_posts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('title');
+            $table->enum('lang', ['id', 'en']);
+            $table->string('slug')->unique();
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
