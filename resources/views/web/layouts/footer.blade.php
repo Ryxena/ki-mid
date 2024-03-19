97a6312da1e429b3a5ee4aa1b3c9daeccb4db7c0
Rubben <147255793+Ryxena@users.noreply.github.com>
2024-03-19 21:40:52 +0700

    Add files via upload

---
+++ resources/views/web/layouts/footer.blade.php
@@ -0,0 +1,46 @@
+<footer>
+    <div class="footer-1 d-flex justify-content-between align-items-center px-5 py-5">
+        <div>
+            <p class="fs-4 fw-semibold text-white m-0">Lebih ringkas dengan aplikasi</p>
+            <p class="text-white">Download aplikasi Helix Laboratorium untuk pengalaman yang lebih ringkas</p>
+        </div>
+        <div>
+            <img src="{{ asset('assets/images/google-play-badge 3.png') }}" alt="">
+            <img src="{{ asset('assets/images/Download_on_the_App_Store_Badge_ID_RGB_blk_100317 1.png') }}"
+                alt="">
+        </div>
+    </div>
+    <div class="footer-2 d-flex m-5 justify-content-between">
+        <div class="footer-logo w-50">
+            <img src="{{ asset('assets/images/Logo.png') }}" alt="">
+            <p class="my-3">
+                Untuk memenuhi permintaan yang meningkat akan layanan kesehatan
+                yang berkualitas. Helix hadir lebih dekat dengan anda.
+            </p>
+            <div class="footer-medsos">
+                <img src="{{ asset('assets/images/Vector-1.png') }}" alt="">
+                <img src="{{ asset('assets/images/Vector-2.png') }}" alt="">
+                <img src="{{ asset('assets/images/Vector.png') }}" alt="">
+            </div>
+        </div>
+        <div class="d-flex justify-content-center w-50 gap-4">
+            <div class="footer-layanan">
+                <h1>Layanan</h1>
+                <ul class="footer-list">
+                    <li>Pemeriksaan berdasarkan penyakit</li>
+                    <li>Pemeriksaan tab baru</li>
+                    <li>Hematologi - Koagulasi</li>
+                </ul>
+            </div>
+            <div class="footer-support">
+                <h1>Support</h1>
+                <ul class="footer-list">
+                    <li><a href="#">Help</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Terms of services</a></li>
+                    <li><a href="#">Privacy policy</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/web/layouts/header.blade.php
@@ -0,0 +1,15 @@
+<nav class="d-flex justify-content-between px-5 py-2 header">
+    <div class="text-center">
+        <img src="{{ asset('assets/images/image 1.png') }}" alt="">
+    </div>
+    <div class="d-flex align-items-center">
+        <ul class="d-flex gap-4 header-list p-0 m-0">
+            <li>
+                <a href="#">Download App</a>
+            </li>
+            <li>
+                <a href="#">Login</a>
+            </li>
+        </ul>
+    </div>
+</nav>
